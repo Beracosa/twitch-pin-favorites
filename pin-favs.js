0366f17cd24c1eecb1ddbb8cc571c91c0760ff24
bxpluse <21994296+bxpluse@users.noreply.github.com>
2021-05-17 19:53:35 -0400

    Update class names

--- pin-favs.js
+++ pin-favs.js
@@ -1,6 +1,6 @@
-const CHANNEL_NAME_CLASS = "tw-c-text-alt tw-ellipsis tw-ellipsis tw-flex-grow-1 tw-font-size-5 tw-line-height-heading tw-semibold";
-const NAVBAR_LIST_CLASS = "tw-relative tw-transition-group";
-const SHOW_MORE_BTN_CLASS = "ScCoreLink-udwpw5-0 fOmFQF tw-link";
+const CHANNEL_NAME_CLASS = "sc-AxirZ eAXpco InjectLayout-sc-588ddc-0 ispdkM";
+const NAVBAR_LIST_CLASS = "InjectLayout-sc-588ddc-0 igUkRe tw-transition-group";
+const SHOW_MORE_BTN_CLASS = "ScCoreLink-udwpw5-0 fNSjov tw-link";
 const RERUN_CLASS = "ScFigure-sc-1j5mt50-0 laJGEQ tw-svg";
 const DEBUGMODE = false;        // Set false for production. Set true for debugging
 const STAR = '⭐';               // Emoji for a star to be displayed next to a pinned channel
